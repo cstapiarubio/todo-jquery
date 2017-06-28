542d5249e5e61d3433fa769a8a5c5000992283a3
cstapiarubio <cstapiarubio@gmail.com>
2017-06-28 15:33:15 -0400

    agregue funcion para añadir tarea

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-main.js
+$(document).ready(function(){
+	$("button").click(function(e){
+		e.preventDefault();
+		var job = $("#tareas").val();
+		if(job == ""){
+			alert("Debes ingresar una tarea");	
+		}
+		else{
+			$("#contenedor").append("<div><span class='center-align'>"+ job + '</span>'+ '</div>');	
+		}
+	});
+});
