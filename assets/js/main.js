0a953873d570ab2bc9948cceb3b84a1e8b632daf
cstapiarubio <cstapiarubio@gmail.com>
2017-06-28 15:50:39 -0400

    adicioné boton a cada tarea

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 			alert("Debes ingresar una tarea");	
 		}
 		else{
-			$("#contenedor").append("<div><span class='center-align'>"+ job + '</span>'+ '</div>');	
+			$("#contenedor").append("<div><span class='center-align'>"+ job + '</span>'+ '</div>'+'<button>Remove</button>');	
 		}
 	});
 });
