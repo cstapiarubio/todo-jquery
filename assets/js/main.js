5322c8f1700cb0afa947c98226c488aa18ef4268
cstapiarubio <cstapiarubio@gmail.com>
2017-06-29 09:29:21 -0400

    intentos de remover las tareas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,47 @@
 $(document).ready(function(){
-	$("button").click(function(e){
+	$("#button").click(function(e){
 		e.preventDefault();
 		var job = $("#tareas").val();
 		if(job == ""){
 			alert("Debes ingresar una tarea");	
 		}
 		else{
-			$("#contenedor").append("<div><span class='center-align'>"+ job + '</span>'+ '</div>'+'<button>Remove</button>'+ "<p><input type='checkbox'></p>");	
+			$("#contenedor").append("<div><span class='center-align'>"+ job + '</span>'+ '</div>'+'<button id="jobRemove">Remove</button>'+ "<p><input type='checkbox'></p>");	
 		}
 	});
 });
+
+  $('#jobRemove').click(function(){
+         $("#contenedor").child().remove();
+    });
+
+  
+
+
+
+
+/*
+$(function() {
+    $("#eliminar").click(function () {
+        $('.check:checked').parent().remove(); 
+    });
+});
+
+
+$(document).ready(function(){
+	$("#jobRemove").click(function(event) {
+			$("#tareas").remove();
+
+			});
+		});
+
+$('#btRemoveAll').click(function() { // Elimina todos los elementos del contenedor
+ 
+$(container).empty();
+$(container).remove();
+$('#btSubmit').remove(); iCnt = 0;
+$('#btAdd').removeAttr('disabled');
+$('#btAdd').attr('class', 'bt');
+ 
+});
+});*/
